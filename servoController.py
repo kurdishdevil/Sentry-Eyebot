33dbd5e5138c327385c9492312a31af6db86c59b
kurdishdevil <44442838+kurdishdevil@users.noreply.github.com>
2020-12-04 21:52:16 +0300

    Update servoController.py

--- servoController.py
+++ servoController.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(08, GPIO.OUT)
-pwm=GPIO.PWM(08, 50)
+GPIO.setup(8, GPIO.OUT)
+pwm=GPIO.PWM(8, 50)
 pwm.start(0)
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(08, True)
+	GPIO.output(8, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(08, False)
+	GPIO.output(8, False)
 	pwm.ChangeDutyCycle(0)
 SetAngle(90) 
 pwm.stop()
