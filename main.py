5e0106e54a8848fa0723a68e87efb3ba74f35cf5
kurdishdevil <44442838+kurdishdevil@users.noreply.github.com>
2020-12-13 21:34:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import servoController
+from datetime import datetime
+import json
+import urllib
+from firebase import Firebase
+config = {
+    apiKey: "AIzaSyBfw-o9Bj5XmwOTOpJI9FjCUqmUZ8R0wwI",
+    authDomain: "sentry-eyebot.firebaseapp.com",
+    databaseURL: "https://sentry-eyebot-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "sentry-eyebot",
+    storageBucket: "sentry-eyebot.appspot.com",
+    messagingSenderId: "778059947065",
+    appId: "1:778059947065:web:1be738fbaaca081d2d01e8",
+    measurementId: "G-7KJYCKWH01"
+}
+firebase = Firebase(config)
+
+while True:
+    db = firebase.database()
+    servoValue = db.child("servo1").get().val()
+    servoController.SetAngle(sevoValue)
