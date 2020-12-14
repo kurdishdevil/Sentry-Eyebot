314af7791b886d2d5aa3440c015f266916a433c7
kurdishdevil <44442838+kurdishdevil@users.noreply.github.com>
2020-12-14 19:11:21 +0300

    Update servoController.py

--- servoController.py
+++ servoController.py
@@ -4,13 +4,3 @@ servo = Servo(14)
 
 def SetAngle(angle):
         servo.value = angle
-        servo.min()
-        sleep(2)
-        servo.mid()
-        sleep(2)
-        servo.max()
-        sleep(2)
-
-
-
-
