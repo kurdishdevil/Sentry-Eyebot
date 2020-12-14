9a61a0582e3c36b78419734cfe9ae1bce4f18a31
kurdishdevil <44442838+kurdishdevil@users.noreply.github.com>
2020-12-14 17:19:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import json
 import urllib
 from firebase import Firebase
 config = {
-    apiKey: "AIzaSyBfw-o9Bj5XmwOTOpJI9FjCUqmUZ8R0wwI",
-    authDomain: "sentry-eyebot.firebaseapp.com",
-    databaseURL: "https://sentry-eyebot-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "sentry-eyebot",
-    storageBucket: "sentry-eyebot.appspot.com",
-    messagingSenderId: "778059947065",
-    appId: "1:778059947065:web:1be738fbaaca081d2d01e8",
-    measurementId: "G-7KJYCKWH01"
+    'apiKey': "AIzaSyBfw-o9Bj5XmwOTOpJI9FjCUqmUZ8R0wwI",
+    'authDomain': "sentry-eyebot.firebaseapp.com",
+    'databaseURL': "https://sentry-eyebot-default-rtdb.europe-west1.firebasedatabase.app",
+    'projectId': "sentry-eyebot",
+    'storageBucket': "sentry-eyebot.appspot.com",
+    'messagingSenderId': "778059947065",
+    'appId': "1:778059947065:web:1be738fbaaca081d2d01e8",
+    'measurementId': "G-7KJYCKWH01"
 }
 firebase = Firebase(config)
 
